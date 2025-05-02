e82b0d02d1cbe1a20d85429bcf95561b91e1c9f1
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-05-02 20:53:35 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,10 @@
 import sys
 import os
-from app.main import app
-import pytest
 
-# Add the 'app' directory to the system path so it can be imported correctly
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Add the 'app' directory (inside 'micro') to the system path so it can be imported correctly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+from main import app  # Assuming main.py is inside the 'app' folder
 
 def test_app():
     # Test if the Flask app is created successfully
