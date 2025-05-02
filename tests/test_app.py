a9c7da685ab6a50c01a3b266dd11fac14b222d8e
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-05-02 20:49:06 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
+import sys
+import os
 from app.main import app
+import pytest
+
+# Add the 'app' directory to the system path so it can be imported correctly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_app():
+    # Test if the Flask app is created successfully
     assert app is not None
