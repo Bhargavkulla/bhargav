1bfcff566fabfa4acadbbda32256b73a8fc1052a
Bhargavkulla <118447823+Bhargavkulla@users.noreply.github.com>
2025-05-02 20:56:51 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,8 @@ import os
 # Add the 'app' directory (inside 'micro') to the system path so it can be imported correctly
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
-from main import app  # Assuming main.py is inside the 'app' folder
-
+from app.main import app
+ 
 def test_app():
     # Test if the Flask app is created successfully
     assert app is not None
